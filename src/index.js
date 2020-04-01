6650c49a090b2207ec8e2f8e49976ee79018ff38
Alexandr <6alex6kash6@gmail.com>
2020-04-01 23:18:08 +0300

    feat: implement renderKeys method

--- src/index.js
+++ src/index.js
@@ -2,110 +2,114 @@ class Keyboard {
   constructor() {
     this.language = 1
     this.capsLock = false
-    this.value = ""
+    this.value = ''
     this.main = null
     this.keysContainer = null
   }
 
   init() {
-    this.main = document.createElement("div")
-    this.keysContainer = document.createElement("div")
+    this.main = document.createElement('div')
+    this.keysContainer = document.createElement('div')
 
-    this.main.classList.add("keyboard")
-    this.keysContainer.classList.add("keyboard__keys")
+    this.main.classList.add('keyboard')
+    this.keysContainer.classList.add('keyboard__keys')
 
     this.keysContainer.appendChild(this.renderKeyboard())
     this.main.appendChild(this.keysContainer)
     document.body.appendChild(this.main)
-    console.log("sss")
+    console.log('sss')
   }
 
-  static renderKeyboard() {
+  renderKeyboard() {
     const fragment = document.createDocumentFragment()
     const keyLayout = [
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "0",
-      "Backspace",
-      "й",
-      "ц",
-      "у",
-      "к",
-      "е",
-      "н",
-      "г",
-      "ш",
-      "щ",
-      "з",
-      "х",
-      "ъ",
-      "Caps Lock",
-      "ф",
-      "ы",
-      "в",
-      "а",
-      "п",
-      "р",
-      "о",
-      "л",
-      "д",
-      "ж",
-      "э",
-      "Enter",
-      "Shift",
-      "я",
-      "ч",
-      "с",
-      "м",
-      "и",
-      "т",
-      "ь",
-      "б",
-      "ю",
-      "?",
-      "space",
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '0',
+      'Backspace',
+      'й',
+      'ц',
+      'у',
+      'к',
+      'е',
+      'н',
+      'г',
+      'ш',
+      'щ',
+      'з',
+      'х',
+      'ъ',
+      'Caps Lock',
+      'ф',
+      'ы',
+      'в',
+      'а',
+      'п',
+      'р',
+      'о',
+      'л',
+      'д',
+      'ж',
+      'э',
+      'Enter',
+      'Shift',
+      'я',
+      'ч',
+      'с',
+      'м',
+      'и',
+      'т',
+      'ь',
+      'б',
+      'ю',
+      '?',
+      'space',
     ]
 
     keyLayout.forEach(key => {
-      const keyElement = document.createElement("button")
-
+      const keyElement = document.createElement('button')
+      keyElement.classList.add('keyboard__key')
       const insertLineBreak
-        = ["Backspace", "ъ", "Enter", "?"].indexOf(key) !== -1
-
-      fragment.appendChild(keyElement)
+        = ['Backspace', 'ъ', 'Enter', '?'].indexOf(key) !== -1
 
       switch (key) {
-        case "Backspace":
-          keyElement.classList.add("keyboard__key--wide")
-          keyElement.innerText = "Backspace"
+        case 'Backspace':
+          keyElement.classList.add('keyboard__key--wide')
+          keyElement.innerText = 'Backspace'
           break
-        case "Caps Lock":
-          keyElement.classList.add("keyboard__key--wide")
-          keyElement.innerText = "Caps Lock"
+        case 'Caps Lock':
+          keyElement.classList.add('keyboard__key--wide')
+          keyElement.innerText = 'Caps Lock'
           break
-        case "space":
-          keyElement.classList.add("keyboard__key--extra--wide")
-          keyElement.innerText = "_________"
+        case 'space':
+          keyElement.classList.add('keyboard__key--extra--wide')
+          keyElement.innerText = '_________'
           break
-        case "Enter":
-          keyElement.classList.add("keyboard__key--wide")
+        case 'Enter':
+          keyElement.classList.add('keyboard__key--wide')
+          keyElement.innerText = 'Enter'
+          keyElement.addEventListener('click', () => {
+            this.value += '/n'
+          })
           break
-        case "Shift":
-          keyElement.classList.add("keyboard__key--wide")
+        case 'Shift':
+          keyElement.classList.add('keyboard__key--wide')
+          keyElement.innerText = 'Shift'
           break
         default:
-          keyElement.classList.add("keyboard__key")
           keyElement.textContent = key.toLowerCase()
+          break
       }
+      fragment.appendChild(keyElement)
       if (insertLineBreak) {
-        fragment.appendChild(document.createElement("br"))
+        fragment.appendChild(document.createElement('br'))
       }
     })
 
@@ -115,6 +119,6 @@ class Keyboard {
 
 const keyboard = new Keyboard()
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   keyboard.init()
 })
